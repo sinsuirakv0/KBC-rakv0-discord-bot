0cbbbbbad7be04b0754f42e5bfef633963ffecf6
健康おじ <239111721+sinsuirakv0@users.noreply.github.com>
2026-03-30 04:49:08 +0900

    Update sukesanping.ts

--- src/commands/sukesanping.ts
+++ src/commands/sukesanping.ts
@@ -1,12 +1,15 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
+import { Command } from "../types/Command";
 import { runPingTest } from "../scheduler/saleScheduler";
 
-const sukesanping = {
+const sukesanping: Command = {
   name: "sukesanping",
   description: "スケジューラの動作確認を行います",
+  usage: "o.sukesanping d | o.sukesanping <HHMM>",
 
-  async execute(message: Message, args: string[]) {
-    await runPingTest(message.client);
+  async execute(message: Message, args: string[]): Promise<void> {
+    const channel = message.channel as TextChannel;
+    await runPingTest(channel, args[0]);
   },
 };
 
