ee3fff37747437c6c056722e75eff851fb40e262
健康おじ <239111721+sinsuirakv0@users.noreply.github.com>
2026-03-30 00:43:34 +0900

    Update sukesanping.ts

--- src/commands/sukesanping.ts
+++ src/commands/sukesanping.ts
@@ -1,16 +1,10 @@
 import { Command } from "../types/Command";
-import { registerPingCommand } from "../scheduler/saleScheduler";
+import { runPingTest } from "../scheduler/saleScheduler";
 
 export const command: Command = {
   name: "sukesanping",
   description: "スケジューラの動作確認を行います",
   async execute(message, args) {
-    // registerPingCommand は messageCreate を監視する関数なので、
-    // ここで一度だけ登録しておく
-    registerPingCommand(message.client);
-
-    // そして「このメッセージをもう一度発火」させて
-    // registerPingCommand 側の処理を実行させる
-    message.client.emit("messageCreate", message);
+    await runPingTest(message.client);
   },
 };
