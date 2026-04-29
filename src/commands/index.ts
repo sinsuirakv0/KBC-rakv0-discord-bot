99f9e03144a2cd94f4fc698dd7fcec30f745d376
健康おじ <239111721+sinsuirakv0@users.noreply.github.com>
2026-04-30 01:36:34 +0900

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,6 +4,6 @@ import commands from "./commands.json";
 export function handleCommand(message: Message, name: string): void {
   const response = (commands as Record<string, string>)[name];
   if (response) {
-    channel.send(response);
+    message.channel.send(response);
   }
 }
