0c7c07cb9a98572fddb0e02e881d1bc7f3fdc815
健康おじ <239111721+sinsuirakv0@users.noreply.github.com>
2026-04-30 01:20:27 +0900

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,9 @@
 import { Message } from "discord.js";
 import commands from "./commands.json";
 
-export function handleCommand(message: Message, name: String): void {
-  const response = (commands as Record<string, string>)[name as string];
+export function handleCommand(message: Message, name: string): void {
+  const response = (commands as Record<string, string>)[name];
   if (response) {
-    message.reply(response);
+    message.channel.send(response);
   }
 }
