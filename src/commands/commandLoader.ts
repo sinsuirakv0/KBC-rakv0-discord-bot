db19a8ef480b3d5d65e5b209f07ac0df954ba85e
健康おじ <239111721+sinsuirakv0@users.noreply.github.com>
2026-04-30 00:17:58 +0900

    Delete src/commands directory

--- src/commands/commandLoader.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export function loadCommands(): Record<string, string> {
-  const filePath = path.join(__dirname, "commands.json");
-  const json = fs.readFileSync(filePath, "utf8");
-  return JSON.parse(json);
-}
